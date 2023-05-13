f6f4249e5fb9bf97385b9a5d0d50343e402f3a77
Dan Howarth <danrhowarth@gmail.com>
2023-05-13 21:00:11 +0100

    halfway through redeployment section

--- fullprocess.py
+++ fullprocess.py
@@ -1,15 +1,9 @@
 import json
 import os
 import re
-import subprocess
 from pathlib import Path
 from scoring import score_model
 
-import training
-import scoring
-import deployment
-import diagnostics
-import reporting
 
 # Load config.json and get input and output paths
 with open('config.json','r') as f:
@@ -18,6 +12,9 @@ with open('config.json','r') as f:
 prod_deploy_path = os.path.join(config['prod_deployment_path'])
 input_folder_path = os.path.join(config['input_folder_path'])
 output_folder_path = config['output_folder_path']
+output_model_path = config['output_model_path']
+
+
 
 ##################Check and read new data
 # first, read ingestedfiles.txt
@@ -46,6 +43,7 @@ if len(common_elements) != len(base_datasets):
     # script needs to run the code in ingestion.py to ingest all the new data.
     os.system('python3 ingestion.py')
 
+    print('getting old and new scores')
     ## check for model drift
     # Read the score from the latest model, recorded in latestscore.txt from the deployment directory,
     # specified in the prod_deployment_path entry of your config.json file.
@@ -56,12 +54,43 @@ if len(common_elements) != len(base_datasets):
     # Make predictions using the trainedmodel.pkl model in the /production_deployment directory
     # and the most recent data you obtained from the previous "Checking and Reading New Data" step.
     # Get a score for the new predictions from step 2 by running the scoring.py.
-    new_score = score_model(output_folder_path, prod_deploy_path, 'finaldata.csv')
+    new_score = score_model(output_folder_path, output_model_path, 'finaldata.csv')
+    ## issue here - prod path is wrong - think i just need to change to output_mdoel_path but just need to dbc
 
     # new_score = subprocess.check_output(['python3', 'scoring.py'])
     # new_score = float(new_score.decode('utf-8').strip())
 
+    print('new: ', new_score)
+    print('old: ', last_score)
 
+    # Check whether the new score from step 3 is higher or lower than the score recorded in latestscore.txt in step 1
+    # using the raw comparison test. If the score from step 3 is lower, then model drift has occurred. Otherwise, it has not.
+    if new_score < last_score:
+        print('model drift has occured')
+
+        print('running training.py')
+        # script needs to run the code in ingestion.py to ingest all the new data.
+        os.system('python3 training.py')
+
+        print('running deployment.py')
+        # script needs to run the code in ingestion.py to ingest all the new data.
+        os.system('python3 deployment.py')
+
+    else:
+        print('no model drift')
+        exit()
+
+    # The last part of your script should run the code from apicalls.py and reporting.py on the most recently
+    # deployed model
+    print('running app.py and apicalls.py')
+    # script needs to run the code in ingestion.py to ingest all the new data.
+    os.system('python3 app.py')
+    # do I need to wait here
+    os.system('python3 apicalls.py')
+
+    print('running reporting.py')
+    # script needs to run the code in ingestion.py to ingest all the new data.
+    os.system('python3 reporting.py')
 
 else:
 
