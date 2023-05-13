611fd2fd0ec932ca38d00f6eb942901432072961
Dan Howarth <danrhowarth@gmail.com>
2023-05-13 15:02:19 +0100

    halfway through final section

--- fullprocess.py
+++ fullprocess.py
@@ -1,4 +1,9 @@
-
+import json
+import os
+import re
+import subprocess
+from pathlib import Path
+from scoring import score_model
 
 import training
 import scoring
@@ -6,15 +11,79 @@ import deployment
 import diagnostics
 import reporting
 
+# Load config.json and get input and output paths
+with open('config.json','r') as f:
+    config = json.load(f)
+
+prod_deploy_path = os.path.join(config['prod_deployment_path'])
+input_folder_path = os.path.join(config['input_folder_path'])
+output_folder_path = config['output_folder_path']
+
 ##################Check and read new data
-#first, read ingestedfiles.txt
+# first, read ingestedfiles.txt
+
+with open(f'{prod_deploy_path}/ingestedfiles.txt', 'r') as base_file:
+    file_contents = base_file.read()
+    base_datasets = re.findall(r'File \d+: .*/(.+\.csv)', file_contents)
+
+directory = Path(input_folder_path)  # replace this with the actual path to your directory
+new_files = directory.glob('*.csv')
+check_files = []
+for file in new_files:
+    check_files.append(file.name)
 
 #second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+# print(check_files, base_datasets)
+# check_files = ['dataset1.csv', 'dataset2.csv']
+set1 = set(check_files)
+set2 = set(base_datasets)
+common_elements = set1.intersection(set2)
+
+# if there has been change, then we need to ingest the new data and check for model drift
+if len(common_elements) != len(base_datasets):
+
+    print('running ingestion.py')
+    # script needs to run the code in ingestion.py to ingest all the new data.
+    os.system('python3 ingestion.py')
+
+    ## check for model drift
+    # Read the score from the latest model, recorded in latestscore.txt from the deployment directory,
+    # specified in the prod_deployment_path entry of your config.json file.
+    with open(f'{prod_deploy_path}/latestscore.txt', 'r') as score_file:
+        last_score = float(score_file.read())
+
+
+    # Make predictions using the trainedmodel.pkl model in the /production_deployment directory
+    # and the most recent data you obtained from the previous "Checking and Reading New Data" step.
+    # Get a score for the new predictions from step 2 by running the scoring.py.
+    new_score = score_model(output_folder_path, prod_deploy_path, 'finaldata.csv')
+
+    # new_score = subprocess.check_output(['python3', 'scoring.py'])
+    # new_score = float(new_score.decode('utf-8').strip())
+
+
+
+else:
+
+    print('data not changed so no need to retain')
+
+    exit()
+
+
+
+
+
+
+
+
+
 
 
 
 ##################Deciding whether to proceed, part 1
 #if you found new data, you should proceed. otherwise, do end the process here
+
+
 
 
 ##################Checking for model drift
--- scoring.py
+++ scoring.py
@@ -19,12 +19,12 @@ test_data_path = os.path.join(config['test_data_path'])
 model_path = os.path.join(config['output_model_path'])
 
 #################Function for model scoring
-def score_model(test_data_path:str, model_path:str):
+def score_model(test_data_path:str, model_path:str, data_file:str='testdata.csv'):
     # this function should take a trained model, load test data, and calculate an F1 score
     # for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    # it should write the result to the latestscore.txt file
 
-    data = pd.read_csv(f'{test_data_path}/testdata.csv')
+    data = pd.read_csv(f'{test_data_path}/{data_file}')
     X = data.drop(columns=['exited', 'corporation'])
     y = data['exited']
 
@@ -37,5 +37,6 @@ def score_model(test_data_path:str, model_path:str):
 
     return f1
 
+
 if __name__ == '__main__':
-    score_model(test_data_path, model_path)
+    f1 = score_model(test_data_path, model_path)
